ac138c92fb5961e4e103dccc03ca2151935d8665
William <unbelievable.half.insane@gmail.com>
2025-09-09 10:59:44 -0700

    Update ItemProperty.java

--- src/william/txtgame/ItemProperty.java
+++ src/william/txtgame/ItemProperty.java
@@ -5,7 +5,14 @@ package william.txtgame;
  * @author William
  */
 public enum ItemProperty {
+	/**This is for when you have a glowing item.
+     * Items with this property ward away grue.
+     */
 	GLOWING,
+	/**This is for frozen items
+	 * I'm not sure what they do yet; I used ChatGPT to make these cause I was tired, and got rid of the dumb ones
+     * I'm not sure if I got rid of all the dumb ones.
+	 */
 	FROZEN,
 	FRAGILE,
 	SHARP,
